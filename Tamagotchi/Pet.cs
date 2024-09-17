3b17f1d2e3777f68ba6cf2b356736f686ae8554a
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2024-09-17 10:17:10 +0200

    done sum stuff

--- Tamagotchi/Pet.cs
+++ Tamagotchi/Pet.cs
@@ -3,4 +3,16 @@ using System;
 public class Pet
 {
 
+private int hunger;
+private int boredom;
+private bool isAlive;
+private List<string> words;
+public string name;
+
+public void Tick()
+{
+    
+}
+
+
 }
--- Tamagotchi/obj/Debug/net8.0/Tamagotchi.AssemblyInfo.cs
+++ Tamagotchi/obj/Debug/net8.0/Tamagotchi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a467da674e41288c0634dcfddc52ecec509888")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
