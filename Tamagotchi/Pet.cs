73a2f233518c2c48e8cbeb1e2d59ff7966148a59
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2024-10-11 13:18:38 +0200

    yes

--- Tamagotchi/Pet.cs
+++ Tamagotchi/Pet.cs
@@ -3,16 +3,78 @@ using System;
 public class Pet
 {
 
-private int hunger;
-private int boredom;
-private bool isAlive;
-private List<string> words;
-public string name;
+    private int hunger;
+    private int boredom;
+    private bool isAlive;
+    private List<string> words = new List<string>();
+    public string name;
+    Random random;
 
-public void Tick()
-{
+    public void Tick()
+    {
+        boredom++;
+        hunger++;
+        if(hunger > 10 || boredom > 10)
+        {
+            isAlive = false;
+        }
+    }
+
+    public void Feed()
+    {
+        hunger--;
+
+        if(hunger <= 0)
+        {
+            hunger = 0;
+        }
+    }
+
+    public void Hi()
+    {
+        if (words.Count < 1)
+        {
+            Console.WriteLine($"they don't know any words");
+        }
+        else
+        {
+            int wordNumber = Random.Shared.Next(words.Count);
+            Console.WriteLine($" {name} says {words[wordNumber]}");
+            ReduceBoredom();
+        }
+    }
+    public void Teach(String word)
+    {
+        Console.WriteLine($"{name} has learned {word}");
+        words.Add(word);
+        ReduceBoredom();
+    }
+
+    public void ReduceBoredom()
+    {
+        boredom--;
+
+        if(boredom <= 0)
+        {
+            boredom = 0;
+        }
+    }
     
+   
+    public void DrawStats()
+    {
+        Raylib.DrawText($"{boredom}", 49, 500, 50, Color.Black);
+    }
+
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
 }
 
 
-}
+
+
+
+
+
--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,1 +1,23 @@
 ﻿global using Raylib_cs;
+
+Pet pet = new();
+
+Raylib.InitWindow(1280, 720, "cool");
+Raylib.SetTargetFPS(60);
+
+while(!Raylib.WindowShouldClose())
+{
+
+    
+    
+    
+    void Save()
+    {
+
+    }
+    
+    Raylib.EndDrawing();
+}
+
+
+
